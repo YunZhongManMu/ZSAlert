9438a313f1b7fcc22edc16003172255134c9b6c9
周顺 <yunzhong1075@126.com>
2017-07-26 20:16:24 +0800

    上传SDK

---
+++ ZSAlerTool/ZSTestSDK.framework/Headers/ZSTestObj.h
@@ -0,0 +1,15 @@
+//
+//  ZSTestObj.h
+//  ZSTestSDK
+//
+//  Created by 周顺 on 2017/7/25.
+//  Copyright © 2017年 AIRWALK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZSTestObj : NSObject
+
++ (void)testMethod;
+
+@end
---
+++ ZSAlerTool/ZSTestSDK.framework/Headers/ZSTestSDK.h
@@ -0,0 +1,20 @@
+//
+//  ZSTestSDK.h
+//  ZSTestSDK
+//
+//  Created by 周顺 on 2017/7/25.
+//  Copyright © 2017年 AIRWALK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZSTestSDK.
+FOUNDATION_EXPORT double ZSTestSDKVersionNumber;
+
+//! Project version string for ZSTestSDK.
+FOUNDATION_EXPORT const unsigned char ZSTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZSTestSDK/PublicHeader.h>
+
+
+#import <ZSTestSDK/ZSTestObj.h>
